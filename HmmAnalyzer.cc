02f4b6031105e983b30549cc7a997d4642b6b533
Irene Dutta <idutta@cern.ch>
2018-10-04 22:54:21 +0200

    fix btag SF

--- HmmAnalyzer.cc
+++ HmmAnalyzer.cc
@@ -240,16 +240,9 @@ void HmmAnalyzer::EventLoop(const char *data,const char *isData)
 	    t_Jet_puId->push_back(Jet_puId[j]);   
             if(Jet_btagDeepB[j]>0.4941){
 	      if(*isData!='T'){
-		double jet_scalefactor    = reader.eval_auto_bounds(
-								    "central", 
-								    BTagEntry::FLAV_B, 
-								    Jet_pt[j], // absolute value of eta
-								    Jet_eta[j]
-								    ); 
-		double jet_scalefactor_up = reader.eval_auto_bounds(
-								    "up", BTagEntry::FLAV_B, Jet_eta[j], Jet_pt[j]);
-		double jet_scalefactor_do = reader.eval_auto_bounds(
-								    "down", BTagEntry::FLAV_B, Jet_eta[j], Jet_pt[j]); 
+		double jet_scalefactor    = reader.eval_auto_bounds("central", BTagEntry::FLAV_B,fabs(Jet_eta[j]), Jet_pt[j]); 
+		double jet_scalefactor_up = reader.eval_auto_bounds("up", BTagEntry::FLAV_B, fabs(Jet_eta[j]), Jet_pt[j]);
+		double jet_scalefactor_do = reader.eval_auto_bounds("down", BTagEntry::FLAV_B, fabs(Jet_eta[j]), Jet_pt[j]); 
 		cout<<jet_scalefactor<<" "<<jet_scalefactor_up<<" "<<jet_scalefactor_do<<endl;
 		t_bJet_SF->push_back(jet_scalefactor);
 		t_bJet_SFup->push_back(jet_scalefactor_up);
