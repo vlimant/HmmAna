ccef6994bd9ad19c171ab5bc033da8472457d447
Nan Lu <nan.lu@cern.ch>
2019-03-08 17:19:06 +0100

    2016

--- HmmAna_stats/run.sh
+++
@@ -1,6 +0,0 @@
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/Data2017_nw2016.root output_Feb12 bkg
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/Glu_nw2016.root output_Feb12 ggH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/VBFH_nw2016.root output_Feb12 VBFH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/WplusH_nw2016.root output_Feb12 WH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/ZH_nw2016.root output_Feb12 ZH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/ttH_nw2016.root output_Feb12 ttH
--- ML/xgboost/README.md
+++
@@ -1,7 +0,0 @@
-This code train the MVA for the Hmm VH-leptonic category (WH(W->ev) process):
-
-1) to train:
-python VHLepTraining_el.py 
-
-2)to add the discriminator output score to the ntuple:
-python EvalBDT_VHLep_el.py /eos/cms/store/user/nlu/Hmm/categorization/Oct03_v1/merged/VBFH.root
