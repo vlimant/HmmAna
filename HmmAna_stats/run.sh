68e7aed499dd9f39843fa671249b9801ad2f1540
Nan Lu <nan.lu@cern.ch>
2019-03-25 18:02:50 +0100

    up

--- HmmAna_stats/run.sh
+++ HmmAna_stats/run.sh
@@ -1,6 +1,19 @@
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/Data2017_nw2016.root output_Feb12 bkg
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/Glu_nw2016.root output_Feb12 ggH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/VBFH_nw2016.root output_Feb12 VBFH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/WplusH_nw2016.root output_Feb12 WH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/ZH_nw2016.root output_Feb12 ZH
-./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Nov12/ws_test/ttH_nw2016.root output_Feb12 ttH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2016/Data2016.root output_March12_2016 bkg
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2016/Glu.root output_March12_2016 ggH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2016/VBFH.root output_March12_2016 VBFH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2016/WH.root output_March12_2016 WH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2016/ZH.root output_March12_2016 ZH
+
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2017/noTrigSF/Data2017.root output_March12_2017 bkg
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2017/noTrigSF/Glu.root output_March12_2017 ggH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2017/noTrigSF/VBFH.root output_March12_2017 VBFH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2017/noTrigSF/WH.root output_March12_2017 WH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2017/noTrigSF/ZH.root output_March12_2017 ZH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2017/noTrigSF/ttH.root output_March12_2017 ttH
+
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2018/Data2018.root output_March12_2018 bkg
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2018/Glu.root output_March12_2018 ggH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2018/VBFH.root output_March12_2018 VBFH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2018/WH.root output_March12_2018 WH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2018/ZH.root output_March12_2018 ZH
+./FitHiggsMuMu /eos/cms/store/user/nlu/Hmm/categorization/Mar11/merged/2018/ttH.root output_March12_2018 ttH
