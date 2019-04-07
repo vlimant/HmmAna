2b2436248f74ce2f1b550b620030077281c57c6c
Nan Lu <nan.lu@cern.ch>
2019-04-07 11:46:07 +0200

    VBF bias study up

--- HmmAna_stats/datacard/VBF_mass_sculpting_check/bias.sh
+++ HmmAna_stats/datacard/VBF_mass_sculpting_check/bias.sh
@@ -1,2 +1,6 @@
-text2workspace.py VBFcat1.txt -o workspace_VBF_cat1.root
-combine -n Obs -M MultiDimFit -m 125 workspace_VBF_cat1.root --algo=singles --robustFit=1 --setParameterRanges r=-50,50 -S 0
+#text2workspace.py VBFcat0.txt -o workspace_VBF_cat0.root
+#combine -n Obs -M MultiDimFit -m 125 workspace_VBF_cat0.root --algo=singles --robustFit=1 --setParameterRanges r=-1,1.5 -S 0
+
+text2workspace.py VBFcat0_bkg.txt -o workspace_VBF_cat0_bkg.root
+combine -n Obs -M MultiDimFit -m 125 workspace_VBF_cat0_bkg.root --algo=singles --robustFit=1 --setParameterRanges r=-1,1.5 -S 0
+#combine -n Obs -M MultiDimFit -m 125 workspace_VBF_cat0.root --algo=singles --robustFit=1 --setRobustFitStrategy 0 --setRobustFitTolerance 1.0 --setParameterRanges r=-50,50 -S 0 -v 2
